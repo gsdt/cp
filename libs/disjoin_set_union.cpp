4a0997cdcc15b463da07e0e86b6f272052fe018b
Tristan Nguyen <tuan.nguyenanhtuan@shopee.com>
2022-09-07 09:27:04 +0800

    add dsu

---
+++ libs/disjoin_set_union.cpp
@@ -0,0 +1,30 @@
+//
+// Created by tuan on 07/09/2022.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+class dsu {
+public:
+    vector<int> p;
+    int n;
+
+    dsu(int _n) : n(_n) {
+        p.resize(n);
+        iota(p.begin(), p.end(), 0);
+    }
+
+    inline int get(int x) {
+        return (x == p[x] ? x : (p[x] = get(p[x])));
+    }
+
+    inline bool unite(int x, int y) {
+        x = get(x);
+        y = get(y);
+        if (x != y) {
+            p[x] = y;
+            return true;
+        }
+        return false;
+    }
+};
